1c66c2f818410d79c72ee40dc01d1fc6f3f001ee
Adi Luhung <luhungadi571@gmail.com>
2025-05-21 20:10:20 +0700

    Jobsheet 10

--- TugasJobsheet10/DataMahasiswa.java
+++ TugasJobsheet10/DataMahasiswa.java
@@ -1,17 +1,17 @@
 package TugasJobsheet10;
 public class DataMahasiswa {
     String nim;
-        String nama;
-        String prodi;
-        String kelas;
+    String nama;
+    String prodi;
+    String kelas;
 
-        public DataMahasiswa(String nim, String nama, String prodi, String kelas) {
-            this.nim = nim;
-            this.nama = nama;
-            this.prodi = prodi;
-            this.kelas = kelas;
-        }
-        public void tampilkanData() {
-            System.out.println(" " + nim + " - " + nama + " - " + prodi + " - " + kelas);
-        }
+    public DataMahasiswa(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+    public void tampilkanData() {
+        System.out.println(" " + nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
 }
